b8fd513e3723c6aa08ac79bac950814428aa38d5
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-04-08 11:42:54 -0500

    Will now be using sqlite for the database purposes. Also Adjusted the file structure and added/removed necessary/unnecessary files.

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
+import sqlite3 from "sqlite3";
 
 const app = express();
 const port = 3000;
+let sql;
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-
-const conn = new pg.Client({
-  host: PGHOST,
-  database: PGDATABASE,
-  user: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: "require",
-  connection: {
-    options: `project=${ENDPOINT_ID}`,
-  },
-});
+const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) return console.error(err.message);
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  try {
-    const result = await conn.query("SELECT * FROM playing_with_neon");
-    console.log(result.rows);
     res.render("index.ejs");
-  } catch (error) {
-    console.error('Error executing query:', error);
-    res.status(500).send('Error fetching data from database');
-  }
-
 });
 
 app.listen(port, () => {
