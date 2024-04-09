5a0df41b3fb99c6fec58a5775f35d819eccaff44
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-04-08 21:25:57 -0500

    created the database table and successfully queried the database from the index.js file. Also made made one todo list in the ejs file and updated some styling.

---
+++ data/queries.sql
@@ -0,0 +1,8 @@
+-- SQLite
+
+-- DROP TABLE IF EXISTS items;
+
+-- CREATE TABLE items (
+--     id INTEGER PRIMARY KEY,
+--     content varchar(100)
+-- );
--- index.js
+++ index.js
@@ -4,18 +4,38 @@ import sqlite3 from "sqlite3";
 
 const app = express();
 const port = 3000;
-let sql;
 
-const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database("./data/database.db", sqlite3.OPEN_READWRITE, (err) => {
   if (err) return console.error(err.message);
 })
+let sql;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-    res.render("index.ejs");
+let items = [];
+
+app.get("/", (res) => {
+  let sql = `SELECT * FROM items`
+  db.all(sql,[], (err, rows) => {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+    items.push(row);
+    });
+  });
+    console.log(items);
+    res.render("index.ejs", {
+      listHeader: "Today",
+      listItems: items,
+    });
+
 });
+
+app.post("/add", async (req, res) => {
+
+});
+
+app.post("/edit", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
